cc1a9f21525eb189f13ff3a1200c7cf3f06cedf7
Bertrand BOUGE <bertrand.bouge@ism-antony.org>
2020-08-08 16:16:41 +0200

    Mise en place de la première class
    PAs facile, car je ne trouve pas encore la bonne doc. Mais cela viendra.
    Existe-t-il en Javascript un type Variant ?
    Mystère...
    Ensuite, il serait préférable de le faire en Python, puisque au programme...
    A voir !

--- build_flat.ts
+++ build_flat.ts
@@ -12,14 +12,34 @@ Version 0.1 2020/08/08
 - Mise en place du squelette de l'aide
 **/
 
-class block_materiau {
-	constructor(m){
-		this.materiau=m;
-	}
+function info(texte:String){
+	/* Cette fonction renvoie le texte passé en varaible */
+    player.say("Le texte est : "+texte);
 }
 
+class b_materiau {
+	/* Cette classe définit les matériaux grace à leur code dans minecraft*/ 
+	constructor(var1:Number){
+		this.materiau=var1;
+	}
+    materiau:Number;
+}
+
+/* Définition des blocs de référence */
 b_sol = new block_materiau(Block.cactus);
 b_mur = new block_materiau(Block.cactus);
 b_escalier = new block_materiau(Block.glay);
 b_vitre = new block_materiau(Block.glass_pane);
 b_toit = new block_materiau(Block.chiseled_stone_bricks)
+
+// l'ajout d'une chaine vide tranfrme le Number en String
+info(b_mur.materiau+"");
+
+/* 
+	la suite est d'avoir un convertisseur coordonnées relatif/absolu
+	Il fautdra aussi avoir un batiment qui peut stocker la mémoire du 
+	point origine de la construction, si on veut la reprendre !
+	Les bâtiments vont être au départ tous diriger vers le nord
+	Allez mon gars, cela avance !
+	
+*/
